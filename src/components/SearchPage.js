e9e8d0062096ca532e874a8028c9717755c97693
abhinavmir <atg271@gmail.com>
2021-04-26 14:10:06 +0530

    more fixes

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,25 +1,23 @@
-import React from 'react';
+import React from "react";
 
+const BarStyling = { margin: "10%" };
 const SearchBar = () => (
+  <div style={BarStyling}>
     <form action="/" method="get">
-        <label htmlFor="header-search">
-            <span className="visually-hidden">Search blog posts</span>
-        </label>
-        <input
-            type="text"
-            id="location"
-            placeholder="Enter location"
-            name="s" 
-        />
+      <label htmlFor="header-search">
+        <span className="visually-hidden">Search blog posts</span>
+      </label>
+      <input type="text" id="location" placeholder="Enter location" name="s" />
 
-<input
-            type="text"
-            id="facility"
-            placeholder="Enter facility required"
-            name="s" 
-        />
-        <button type="submit">Search</button>
+      <input
+        type="text"
+        id="facility"
+        placeholder="Enter facility required"
+        name="s"
+      />
+      <button type="submit">Search</button>
     </form>
+  </div>
 );
 
 export default SearchBar;
