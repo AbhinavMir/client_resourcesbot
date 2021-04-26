69edca87d59855269aa6f40db0f2b2db7fb5b65d
abhinavmir <atg271@gmail.com>
2021-04-26 14:04:57 +0530

    more fixes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 //Calling Firebase config setting to call the data
 import firebase from "./Firebase";
 import ListDatabase from './components/ListDatabase.js'
-
+import SearchPage from './components/SearchPage.js'
 
 function App() {
   return (
     <div className="App">
+      <SearchPage />
       <ListDatabase />
     </div>
   );
